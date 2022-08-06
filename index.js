580c020e76188c7c1604a492ab0b6702601ea53d
namnh <namnn@vn-sis.com>
2022-08-07 02:31:06 +0700

    test

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ app.get('/', (req, res) => {
 	res.status(200);
 	res.send('Welcome to root URL of Server1');
 });
-app.get('/csv', async (req, res) => {
+app.get('/torrent', async (req, res) => {
 	try {
 		const date = req.query?.date;
-		const type = req.query?.type;
-		console.log({ date });
-		console.log({ type });
-
 		let start = 0;
 		let end = 200;
 		let host = `https://jav-torrent.org/date/${moment(date).format(
@@ -32,9 +28,7 @@ app.get('/csv', async (req, res) => {
 		try {
 			let data = [];
 			for (let j = parseInt(start); j < parseInt(end); j++) {
-				console.log('url', url(j + 1));
 				const html = await rp(url(j + 1));
-				console.log('hehe');
 				const dom = new JSDOM(`${html}`);
 				var arr = [],
 					l = dom.window.document.links;
@@ -55,10 +49,58 @@ app.get('/csv', async (req, res) => {
 				return { url: item };
 			});
 
-			console.log({ mapping });
 			const csv = new ObjectsToCsv(mapping);
 			await csv.toDisk('./test.csv');
-			console.log('path', path.join(__dirname, 'test.csv'));
+			return res.sendFile(path.join(__dirname, 'test.csv'));
+		} catch (e) {
+			console.log({ e });
+			res.status(200).json({ data: 'error' });
+		}
+		return res.status(200).json({ data: [] });
+	} catch (e) {
+		return res.status(200).json({ data: [] });
+	}
+});
+
+app.get('/jav', async (req, res) => {
+	try {
+		const date = req.query?.date;
+		let start = 0;
+		let end = 200;
+		let host = `https://www.141jav.com/date/${moment(date).format(
+			'YYYY/MM/DD'
+		)}?page=`;
+		const url = (index) => {
+			return `${host}${index}`;
+		};
+		try {
+			let data = [];
+			for (let j = parseInt(start); j < parseInt(end); j++) {
+				console.log('asdasd', url(j + 1));
+				const html = await rp(url(j + 1));
+
+				const dom = new JSDOM(`${html}`);
+				var arr = [],
+					l = dom.window.document.links;
+
+				for (var i = 0; i < l.length; i++) {
+					arr.push(l[i].href);
+				}
+				const breakPage = arr.find((item) => item.includes('/date/'));
+				if (!breakPage) {
+					break;
+				}
+
+				const needArr = arr.filter((item) => item.includes('/download/'));
+				const haveDomain = needArr.map((item) => item);
+				data = [...data, ...haveDomain];
+			}
+			const mapping = data.map((item, index) => {
+				return { url: item };
+			});
+
+			const csv = new ObjectsToCsv(mapping);
+			await csv.toDisk('./test.csv');
 			return res.sendFile(path.join(__dirname, 'test.csv'));
 		} catch (e) {
 			console.log({ e });
